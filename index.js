e6b7789560215dbf8f576db1db079e6283a7da63
Nakul dev Khajuria <nakuldevkhajuria@gmail.com>
2022-11-10 16:35:38 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,64 @@
+// const http = require('http')
+const express = require('express')
+const app = express();
+
+
+
+const middleware = (req,res,next) => {
+res.write('i am inside first middleware  ')
+
+    next();
+}
+
+app.use(middleware);//this middleware will be globally declared
+
+const middleware2 = (req,res,next) => {
+    const result = 5;
+if (result === 5) {
+    res.write('we are insdie middleware2  ')
+ 
+    next();
+   
+   
+} else {
+  res.send('We are having some errror to compelte the app')
+}
+    
+}
+
+app.get('/home',middleware2,
+(req,res) => {
+    console.log(req.body);
+    res.write('homeRouteIsWorking, and we are using local middleware');
+    res.end();
+})
+app.get('/home2',middleware2,
+(req,res) => {
+    console.log(req.body);
+    res.write('we are at home, and we are using local middleware');
+    res.end();
+})
+app.post('/work',
+(req,res) => {
+    
+
+    // res.write(req.body);
+    res.write('We are in work and we are using global middleware' );
+    res.end();
+})
+app.get('/travel',
+(req,res) => {
+    
+    res.write('We are traveling, and we are using global middleware');
+    res.end();
+})
+app.get('/backHome',
+(req,res) => {
+    
+    res.write('We are backHome and we are using global middleware');
+    res.end();
+})
+
+app.listen(5000,() => {
+    console.log('App is started');
+})
